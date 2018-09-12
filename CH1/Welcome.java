8ec92bed497ddf0fcbaf1800ff19074500b63c1e
EdwardC2J <yywzc2j@163.com>
2018-09-12 11:51:42 +0800

    Add files via upload

---
+++ CH1/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {//定义 类(class)
+	public static void main(String[] args) {//定义 主方法(main method)
+		//Display message to the Console/注释(comment)
+		System.out.println("Welcome to Java!");
+	}
+}
---
+++ CH1/WelcomeInMessageDialogBox.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class WelcomeInMessageDialogBox {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Welcome to Java!");
+		JOptionPane.showMessageDialog(null, "Welcome to Java!","Display Messgae", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
