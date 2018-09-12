f75ffa57d26b055d4c44018c74afec4a2f278f8c
EdwardC2J <yywzc2j@163.com>
2018-09-12 14:22:47 +0800

    Add files via upload

---
+++ CH2/Code/DisplayTime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DisplayTime {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter an integer for seconds: ");
+		int seconds=input.nextInt();
+		int minutes=seconds/60;
+		int remainingSeconds=seconds%60;
+		System.out.println(seconds+" second is "+minutes+" minutes and "
+				+remainingSeconds+" seconds ");
+		System.out.println(1.0-0.9);
+	}
+}
